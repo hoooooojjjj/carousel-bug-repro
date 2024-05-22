217f49a3c405f4ef787f2ec1935f44f972ddc5d8
sono <rhj080471@gmail.com>
2024-05-22 23:02:29 +0900

    add comment

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ const App = () => {
   };
 
   return (
+    /* I add props, arrows={true} 
+    -> console said :
+    1. console.js:273 Warning: React does not recognize the `currentSlide` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `currentSlide` instead. If you accidentally passed it from a parent component, remove it from the DOM element.
+    2. console.js:273 Warning: React does not recognize the `slideCount` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `slideCount` instead. If you accidentally passed it from a parent component, remove it from the DOM element. */
+
     <Carousel afterChange={onChange} arrows={true}>
       <div>
         <h3 style={contentStyle}>1</h3>
